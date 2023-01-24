c5a8a8aea537104b56de7ef74e51678bec8515b7
Marco Amador <marcoamador214@gmail.com>
2023-01-24 14:25:45 -0800

    implemented instagram posts and links

--- client/src/Gallery.js
+++ client/src/Gallery.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 import SubHeading from './SubHeading';
-import images from './constants/images';
+import { images } from './constants';
 import './Gallery.css';
+import InstagramEmbed from 'react-instagram-embed';
+
+const galleryImages = [images.Post1, images.Post2, images.Post4, images.Post5]
 
 function Gallery() {
-const scrollRef = React.useRef(null)
+const scrollRef = React.useRef(null);
 
 const scroll = (direction) => {
   const { current } = scrollRef;
@@ -21,13 +24,22 @@ const scroll = (direction) => {
   return (
     <div className='app__gallery flex__center'>
       <div className='app__gallery-content'>
-        <SubHeading title='Instagram'/>
-        <h1 className='headtext__cormorant'>Latest Posts</h1>
-        <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem'}}>Lorem Ipsum Dolor</p>
+        <SubHeading title='@calbearsfball'/>
+        <h1 className='headtext__cormorant'>Latest IG Posts</h1>
+        <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem'}}>Go Bears!</p>
         <button type='button' className='custom__button'>View More</button>
       </div>
       <div className='app__gallery-images'>
-        <div className='app__gallery-images-container' ref={scrollRef}></div>
+        <div className='app__gallery-images_container' ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}>
+              <img src={image} alt='post'/>
+              <a className='gallery__image-icon' href='https://www.instagram.com/p/CnkUggAv8yh/?hl=en'>
+                <BsInstagram className='gallery__image-icon'/>
+              </a>
+            </div>
+          ))}
+        </div>
         <div className='app__gallery-images_arrows'>
           <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')}/>
           <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')}/>
--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -4,6 +4,11 @@ import JaydnPoty from '../assets/JaydnPoty.jpg';
 import danielPoty from '../assets/danielPoty.jpg';
 import jaydnMvp from '../assets/jaydnMvp.jpg';
 import jerbo from '../assets/jerbo.jpg';
+import Post1 from '../assets/Post1.jpg';
+import Post2 from '../assets/Post2.jpg';
+import Post3 from '../assets/Post3.jpg';
+import Post4 from '../assets/Post4.jpg';
+import Post5 from '../assets/Post5.jpg';
 
 export default {
     CalbearsFball,
@@ -11,5 +16,10 @@ export default {
     JaydnPoty,
     danielPoty,
     jaydnMvp,
-    jerbo
+    jerbo,
+    Post1,
+    Post2,
+    Post3,
+    Post4,
+    Post5
 }
